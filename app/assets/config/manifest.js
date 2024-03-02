10c7f72217c5fdcd33eacab40cac6c688f060187
TakuyaTaniguchi <makubex.tt@gmail.com>
2024-03-02 20:57:28 +0900

    不要なJsでエラーが出ているのでコメントアウト？

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+
+// 不要？
+// = link_tree ../../../vendor/bundle/javascript .js
